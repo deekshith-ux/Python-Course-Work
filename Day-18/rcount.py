e588f329ccc4e5f3168d62ccf1e07acd3bed07c8
Adepu Dhikshith <adepubunny420@gmail.com>
2026-06-23 11:44:56 +0530

    Add files via upload

---
+++ Day-18/rcount.py
@@ -0,0 +1,43 @@
+'''
+s=4567
+count=0
+for i in str(s):
+    count+=1
+print(count)
+
+
+def display(s,ind):
+    if ind==len(s):
+        return
+    print(s[:ind+1])
+    display(s,ind+1)
+display("python",0)
+
+
+def display(s,ind,l):
+    if ind==len(s)-l+l:
+        return
+    print(s[ind:ind+l])
+    display(s,ind+1,l)
+display("abcdefg",0,3)
+
+    
+def display(l,ind):
+    if ind==len(l):
+        return 0
+    return l[ind]+display(l,ind+1)
+l=[1,2,3,4,5,6,7]
+print(display(l,0))
+
+'''
+
+def display(s,i):
+    if i==len(s):
+        return 0
+    if s[i] in 'aeiouAEIOU':
+        return 1+display(s,i+1)
+    else:
+        return display(s,i+1)
+    
+s="python programming"
+print(display(s,0))
